c758d2986c1ea647f38fb291aaea2c9b83b25c60
Long <fchbyoung1010@gmail.com>
2019-06-01 10:45:50 +0700

    passed fizz test

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,10 @@
 function fizzBuzz(number) {
     const isNumberEqualBy10 = number === 10;
-    const TEN = 10;
+    const TEN = '10';
+    const isDiviableBy3 = number % 3 === 0;
     if (isNumberEqualBy10) {
         return TEN;
-    } else if (number % 3 === 0) {
-        return 'Fizz';
+    } else if (isDiviableBy3) {
+        return isDiviableBy3;
     }
 }

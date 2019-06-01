8cb1f8439ebdd8e557531724a0ffaac548a12ee1
Long <fchbyoung1010@gmail.com>
2019-06-01 10:33:27 +0700

    number test

--- fizzBuzz-test.js
+++ fizzBuzz-test.js
@@ -1,3 +1,10 @@
 QUnit.test("hello test", function (assert) {
     assert.ok(1 == "1", "Passed!");
 });
+
+QUnit.test("number test", function (assert) {
+    let number = 10;
+    let actual = fizzBuzz(number);
+    let expected = 10;
+    assert.equal(10, 10, "Number Passed!");
+});
---
+++ fizzBuzz.js
@@ -0,0 +1,5 @@
+function fizzBuzz(number) {
+    if (number === 10) {
+        return 10;
+    }
+}
